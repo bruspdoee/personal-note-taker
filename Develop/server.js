6f77db7df805e8552452f3e583c0f669500af8c6
Brus Palaj <brus.palaj71@gmail.com>
2020-04-20 18:59:39 -0400

    added routes for api and html

--- Develop/server.js
+++ Develop/server.js
@@ -1,15 +1,17 @@
 const express = require("express"); 
+const apiRoutes = require("./routes/apiRoutes"); 
+const htmlRoutes = require("./routes/htmlRoutes");
 
 const PORT = process.env.PORT || 3000; 
 
 const app = express(); 
 
-app.get((req, res) => {
-    console.log(req.url); 
-    res.end("Finished"); 
-})
+app.use(express.json()); 
+app.use(express.urlencoded()); 
 
-
+// routes 
+apiRoutes(app); 
+htmlRoutes(app); 
 
 app.listen(PORT, () => {
     console.log("Listening..")
