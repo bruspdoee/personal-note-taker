f39289d5c2fa7b6e1bcdcbd17c44c714f470c50e
Brus Palaj <brus.palaj71@gmail.com>
2020-06-17 22:17:28 -0400

    server change

--- server.js
+++ server.js
@@ -1,71 +1,71 @@
-const express = require("express"); 
-const path = require("path"); 
-const db = require("./db/db.json"); 
-const fs = require("fs"); 
+const express = require("express");
+const path = require("path");
+const db = require("./db/db.json");
+const fs = require("fs");
 
-const PORT = 3000; 
+const PORT = 3000;
 
-const app = express(); 
+const app = express();
 
-app.use(express.json()); 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html")); 
-})
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html")); 
-})
-
-app.get("/api/notes", function (req, res) {
-    // send a note
-    res.json(db); 
-}); 
-
- //post notes
-app.post("/api/notes", function (req, res) {
-     // add a note
-    var note = req.body; 
-    fs.readFile("./db/db.json", function (err, data) {
-        if (err) throw err;
-        var newArr = JSON.parse(data); 
-        note["id"] = newArr.length; 
-        newArr.push(note); 
-
-        var newFile = JSON.stringify(newArr); 
-
-        fs.writeFile("./db/db.json", newFile, function (err) {
-            if (err) throw err; 
-            return data; 
-        })
-        res.json(db); 
-
-        // function to refresh list 
-    })
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
- //delete notes
-app.delete("/api/notes/:id", function (req, res) {
-    var deleteNote = req.params.id; 
-    var arr = db; 
-    for(var i = 0; i < arr.length; i++) {
-        if (deleteNote == arr[i].id) {
-            arr.splice(i, 1); 
-            return arr 
-        }
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
-        var rewrite = JSON.stringify(arr); 
-        fs.writeFile("./db/db.json", rewrite, function (err) {
-            if (err) throw err; 
-            console.log("deleted"); 
-        })
+app.get("/api/notes", function (req, res) {
+  // send a note
+  res.json(db);
+});
+
+//post notes
+app.post("/api/notes", function (req, res) {
+  // add a note
+  var note = req.body;
+  fs.readFile("./db/db.json", function (err, data) {
+    if (err) throw err;
+    var newArr = JSON.parse(data);
+    note["id"] = newArr.length;
+    newArr.push(note);
+
+    var newFile = JSON.stringify(newArr);
+
+    fs.writeFile("./db/db.json", newFile, function (err) {
+      if (err) throw err;
+      return data;
+    });
+    res.json(db);
+
+    // function to refresh list
+  });
+});
+
+//delete notes
+app.delete("/api/notes/:id", function (req, res) {
+  var deleteNote = req.params.id;
+  var arr = db;
+  for (var i = 0; i < arr.length; i++) {
+    if (deleteNote == arr[i].id) {
+      arr.splice(i, 1);
+      return arr;
     }
 
-    // function to refresh list 
-}); 
+    var rewrite = JSON.stringify(arr);
+    fs.writeFile("./db/db.json", rewrite, function (err) {
+      if (err) throw err;
+      console.log("deleted");
+    });
+  }
 
-app.listen(PORT, () => {
-    console.log("Listening...")
-}); 
+  // function to refresh list
+});
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log("Listening...");
+});
